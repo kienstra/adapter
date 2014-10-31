5484c62ccc3437db7017d777d18d6cbb5ac9132d
Ryan Kienstra <kienstraryan@gmail.com>
2014-10-31 13:56:26 -0700

    removed space before closing ;

--- content-page.php
+++ content-page.php
@@ -1,13 +1,13 @@
-<?php defined('ABSPATH') or die( "No direct access!" ) ; ?>
+<?php defined('ABSPATH') or die( "No direct access!" ); ?>
 
-<article id="post-<?php esc_attr( the_ID() ) ; ?>" <?php esc_attr( post_class( 'post' ) ) ; ?>>
+<article id="post-<?php esc_attr( the_ID() ); ?>" <?php esc_attr( post_class( 'post' ) ); ?>>
   <h1>
-    <?php the_title( '<header class="entry-header"><h1 class="entry-title">', awp_maybe_echo_edit_link() . '</h1></header>' ) ; ?>
+    <?php the_title( '<header class="entry-header"><h1 class="entry-title">', awp_maybe_echo_edit_link() . '</h1></header>' ); ?>
   </h1>
   <div class="entry-content">     
-    <?php the_content() ;
-    awp_custom_wp_link_pages() ;
-    awp_display_comment_form_or_template() ;       
+    <?php the_content();
+    awp_custom_wp_link_pages();
+    awp_display_comment_form_or_template();       
     ?>
   </div>
 </article>
