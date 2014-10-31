ebb5cf027eabc1ef7cd761eebf4f7e36fec6b6c9
Ryan Kienstra <kienstraryan@gmail.com>
2014-10-31 13:45:22 -0700

    replaced htmlspecialchars with esc_html

--- searchform.php
+++ searchform.php
@@ -1,9 +1,9 @@
-<?php $search_terms = isset( $_GET[ 's' ] ) ? htmlspecialchars( $_GET[ 's' ] ) : '' ; ?>
+<?php $search_terms = isset( $_GET[ 's' ] ) ?  $_GET[ 's' ] : '' ; ?>
 
   <form role="form" action="<?php echo esc_url( home_url() ) ; ?>" method="get" id="search-form">
   <label for="s" class="sr-only"><?php _e( 'Search' , 'adapter-wp' ) ; ?></label>
   <div class="input-group">
-    <input type="text" id="s" class="form-control" name="s" placeholder="Search" <?php if ( '' !== $search_terms ) { echo ' value="' . $search_terms . '"' ; } ?> />
+    <input type="text" id="s" class="form-control" name="s" placeholder="Search" <?php if ( '' !== $search_terms ) { echo ' value="' . esc_html( $search_terms ) . '"' ; } ?> />
     <div class="input-group-btn">
       <button type="submit" class="btn btn-primary btn-med">
 	<span class="glyphicon glyphicon-search"></span>
