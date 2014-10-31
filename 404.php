d66bf415d02aa330e3ea69b07c44a94b990a4dcf
Ryan Kienstra <kienstraryan@gmail.com>
2014-10-31 15:59:53 -0700

    Indents instead of spaces

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php
 
 get_header();
-  get_template_part( 'no-post-found' );
-  get_template_part( 'awp-posts-and-pages' ); 
+	get_template_part( 'no-post-found' );
+	get_template_part( 'awp-posts-and-pages' ); 
 get_footer();
