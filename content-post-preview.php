82d44d6e1101a239f8ffceb876e8b0181a2c887f
Ryan Kienstra <kienstraryan@gmail.com>
2014-10-31 16:04:09 -0700

    Change spaces to indents

--- content-post-preview.php
+++ content-post-preview.php
@@ -1,28 +1,28 @@
 <?php defined('ABSPATH') or die( "No direct access!" ); ?>
 
 <article <?php esc_attr( post_class( 'post-preview' ) ); ?>>
-  <a href="<?php the_permalink(); ?>">
-    <?php the_post_thumbnail(
-            array( 300 , 200 ) ,  
-            array( 'class' => 'pull-right img-rounded home-wp-post-image home-featured-image' )
-	  );  
-    ?>
-  </a>
-  <h2>
-    <a href="<?php the_permalink(); ?>"><?php the_title_attribute(); ?></a>
-  </h2>
-  <p>
-      <?php awp_author_date_category_tag(); ?>
-      <a href="<?php the_permalink(); ?>" class="mobile-post-permalink">
+	<a href="<?php the_permalink(); ?>">
+		<?php the_post_thumbnail(
+		      array( 300 , 200 ) ,	
+		      array( 'class' => 'pull-right img-rounded home-wp-post-image home-featured-image' )
+		);	
+		?>
+	</a>
+	<h2>
+		<a href="<?php the_permalink(); ?>"><?php the_title_attribute(); ?></a>
+	</h2>
+	<p>
+		<?php awp_author_date_category_tag(); ?>
+		<a href="<?php the_permalink(); ?>" class="mobile-post-permalink">
 	<span class="glyphicon glyphicon-chevron-right"></span>
-      </a>
-  </p>
-  <p>
-    <?php echo get_the_excerpt(); ?>
-    <a class="pull-right btn btn-primary btn-med read-more-button" href="<?php the_permalink(); ?>">
-      <?php _e( 'Read more' , 'adapter-wp' ); ?><span class="glyphicon glyphicon-chevron-right"></span>
-    </a>
-    <div class="clearfix"></div>
-  </p>
-  <hr>
+		</a>
+	</p>
+	<p>
+		<?php echo get_the_excerpt(); ?>
+		<a class="pull-right btn btn-primary btn-med read-more-button" href="<?php the_permalink(); ?>">
+			<?php _e( 'Read more' , 'adapter-wp' ); ?><span class="glyphicon glyphicon-chevron-right"></span>
+		</a>
+		<div class="clearfix"></div>
+	</p>
+	<hr>
 </article>
--- header.php
+++ header.php
@@ -2,16 +2,16 @@
 
 <!DOCTYPE html> 
 <html <?php language_attributes( 'html' ); ?>>
-  <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" >
-    <title><?php wp_title(); ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" >
-    <!-- style.css enqueued in functions.php using get_stylesheet_uri() -->
-    <?php wp_head(); ?>
-  </head>
-  <body <?php echo body_class(); ?>>
-    <?php do_action( 'awp_before_top_nav' ); ?>
-    <?php awp_maybe_get_top_nav(); ?>
-    <div class="container">
-      <?php do_action( 'awp_top_of_page' ); ?>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" >
+		<title><?php wp_title(); ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" >
+		<!-- style.css enqueued in functions.php using get_stylesheet_uri() -->
+		<?php wp_head(); ?>
+	</head>
+	<body <?php echo body_class(); ?>>
+		<?php do_action( 'awp_before_top_nav' ); ?>
+		<?php awp_maybe_get_top_nav(); ?>
+		<div class="container">
+			<?php do_action( 'awp_top_of_page' ); ?>
