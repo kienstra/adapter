a5107f3dd61303e4f07559443b8c7a1c78a223e2
Ryan Kienstra <kienstraryan@gmail.com>
2014-10-31 13:40:28 -0700

    Removed needless esc_* functions

--- header.php
+++ header.php
@@ -1,16 +1,16 @@
 <?php defined( 'ABSPATH' ) or die( "No direct access!" ) ; ?>
 
 <!DOCTYPE html> 
-<html <?php esc_attr( language_attributes( 'html' ) ) ; ?>>
+<html <?php language_attributes( 'html' ) ; ?>>
   <head>
-    <meta charset="<?php esc_attr( bloginfo( 'charset' ) ) ; ?>" >
+    <meta charset="<?php bloginfo( 'charset' ) ; ?>" >
     <title><?php wp_title(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="pingback" href="<?php esc_url( bloginfo( 'pingback_url' ) ) ; ?>" >
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ) ; ?>" >
     <!-- style.css enqueued in functions.php using get_stylesheet_uri() -->
     <?php wp_head() ; ?>
   </head>
-  <body <?php echo esc_attr( body_class() ) ; ?>>
+  <body <?php echo body_class() ; ?>>
     <?php do_action( 'awp_before_top_nav' ) ; ?>
     <?php awp_maybe_get_top_nav() ; ?>
     <div class="container">
