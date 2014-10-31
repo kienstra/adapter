dd47842f61c7e6d30764c0f6fc802502a6473a9c
Ryan Kienstra <kienstraryan@gmail.com>
2014-10-31 16:08:29 -0700

    Indentations instead of spaces

--- home.php
+++ home.php
@@ -2,14 +2,14 @@
 
 <?php get_header(); ?>
 <div class="row">
-  <div class="col-md-8">
-    <?php awp_the_breadcrumbs(); ?>  
-    <h1><?php wp_title( '' ); ?></h1>
-    <?php awp_query_for_post_previews(); ?>
-  </div>
-  <div class="col-md-4 main-sidebar">
-    <?php wp_meta(); ?>
-    <?php if ( dynamic_sidebar( 'main_sidebar' ) ); ?>
-  </div>
+	<div class="col-md-8">
+		<?php awp_the_breadcrumbs(); ?>	 
+		<h1><?php wp_title( '' ); ?></h1>
+		<?php awp_query_for_post_previews(); ?>
+	</div>
+	<div class="col-md-4 main-sidebar">
+		<?php wp_meta(); ?>
+		<?php if ( dynamic_sidebar( 'main_sidebar' ) ); ?>
+	</div>
 </div>
 <?php get_footer(); ?>
