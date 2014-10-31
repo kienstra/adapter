af2636f64967a9397bd39fa23839254258564a6b
Ryan Kienstra <kienstraryan@gmail.com>
2014-10-31 13:52:03 -0700

    removed ?> at end of files, moved closing semicolons

--- 404.php
+++ 404.php
@@ -1,7 +1,6 @@
 <?php
 
-get_header() ;
-  get_template_part( 'no-post-found' ) ;
-  get_template_part( 'awp-posts-and-pages' ) ; 
-get_footer() ;
-?>
+get_header();
+  get_template_part( 'no-post-found' );
+  get_template_part( 'awp-posts-and-pages' ); 
+get_footer();
