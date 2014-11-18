c615f06d5dfa025de431b6fc0fc1a8213d798629
Ryan Kienstra <kienstraryan@gmail.com>
2014-11-17 21:32:12 -0800

    removed type => option

--- includes/awp-customizer.php
+++ includes/awp-customizer.php
@@ -21,7 +21,6 @@ function awp_add_customizer_sections( $wp_customize ) {
 	$wp_customize->add_setting( 'awp_banner_background_color' , array(
 		'default' => 'F8F8F8' ,
 		'capability' => 'edit_theme_options' ,
-		'type' => 'option' ,
 		'transport' => 'postMessage' ,
 		'sanitize_callback' => 'awp_sanitize_customizer_value' ,
 	) );
@@ -30,7 +29,7 @@ function awp_add_customizer_sections( $wp_customize ) {
 		$wp_customize ,
 		'banner_background_color' ,
 		array(
-			'label' => __( 'Header Backround Color' , 'adapter-wp' ) ,
+			'label' => __( 'Backround Color' , 'adapter-wp' ) ,
 			'section' => 'top_banner_background_color' ,
 			'settings' => 'awp_banner_background_color' ,
 		)
--- top-banner.php
+++ top-banner.php
@@ -2,7 +2,7 @@
 
 <?php if ( '' != get_header_image() ) : ?>
 	<a class="header-banner-link" href="<?php echo esc_url( home_url() ); ?>">
-		<div class="header-banner-wrapper" style="background:#<?php echo esc_attr( get_theme_mod( 'awp_banner_background_color' ) ); ?>">
+		<div class="header-banner-wrapper" style="background:<?php echo esc_attr( get_theme_mod( 'awp_banner_background_color' ) ); ?>">
 			<div class="header-banner" style="background:url(<?php esc_attr( header_image() ); ?>) no-repeat <?php awp_the_top_banner_backround_alignment() ?>; height : <?php echo esc_attr( get_custom_header()->height ); ?>px;" >
 			</div>
 		</div>
