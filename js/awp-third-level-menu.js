7db1257908f0e745e1bd9b2b3f413e9f147d1ef8
Ryan Kienstra <kienstraryan@gmail.com>
2014-12-17 11:17:14 -0300

    js/awp-third-level-menu.js: support for third-level menus

---
+++ js/awp-third-level-menu.js
@@ -0,0 +1,30 @@
+( function( $ ) {
+	$( function() {
+		var selectorForThirdLevelMenuItem = '.menu-item-has-children .menu-item-has-children',
+		    $parentOfThirdLevelMenuItems = $( selectorForThirdLevelMenuItem ),
+		    $firstLevelMenuItemWithChildren = $parentOfThirdLevelMenuItems.parents( '.dropdown' ),
+		    cssValuesForHidden =
+			{
+			  'display' : 'none' ,
+			  'opacity' : 0
+			};
+
+		$parentOfThirdLevelMenuItems.find( 'ul.dropdown-menu' )
+			.removeClass( 'dropdown-menu' )
+			.addClass( 'third-level-menu' )
+			.css( cssValuesForHidden );
+
+		$parentOfThirdLevelMenuItems.on( 'mouseenter' , function() {
+			$( this ).find( '.third-level-menu' )
+				 .css( 'display' , 'block' )
+				 .animate( { 'opacity' : 1 } , 200 );
+		});
+
+		$firstLevelMenuItemWithChildren.on( 'mouseleave' , function() {
+			$( this ).find( '.third-level-menu' )
+				 .css( cssValuesForHidden );
+		});
+
+	});
+
+}( jQuery) );
