64d222065cfbf09014f6cb35eedd6e149d253876
Ryan Kienstra <kienstraryan@gmail.com>
2015-01-16 16:45:39 -0400

    escaping output

--- content-page.php
+++ content-page.php
@@ -1,6 +1,6 @@
 <?php defined('ABSPATH') or die( "No direct access!" ); ?>
 
-<article id="post-<?php esc_attr( the_ID() ); ?>" <?php esc_attr( post_class( 'post' ) ); ?>>
+<article id="post-<?php esc_attr( get_the_ID() ); ?>" <?php esc_attr( post_class( 'post' ) ); ?>>
 	<h1>
 		<?php the_title( '<header class="entry-header"><h1 class="entry-title">' , awp_maybe_echo_edit_link() . '</h1></header>' ); ?>
 	</h1>
--- content-post-preview.php
+++ content-post-preview.php
@@ -1,26 +1,26 @@
 <?php defined('ABSPATH') or die( "No direct access!" ); ?>
 
 <article <?php esc_attr( post_class( 'post-preview' ) ); ?>>
-	<a href="<?php the_permalink(); ?>">
+	<a href="<?php esc_url( get_the_permalink() ); ?>">
 		<?php the_post_thumbnail(
-		      array( 300 , 200 ) ,	
-		      array( 'class' => 'pull-right img-rounded home-wp-post-image home-featured-image' )
-		);	
+				array( 300 , 200 ) ,	
+				array( 'class' => 'pull-right img-rounded home-wp-post-image home-featured-image' ) );	
 		?>
 	</a>
 	<h2>
-		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		<a href="<?php esc_url( get_the_permalink() ); ?>"><?php the_title(); ?></a>
 	</h2>
 	<p>
 		<?php awp_author_date_category_tag(); ?>
-		<a href="<?php the_permalink(); ?>" class="mobile-post-permalink">
+		<a href="<?php esc_url( get_the_permalink() ); ?>" class="mobile-post-permalink">
 	<span class="glyphicon glyphicon-chevron-right"></span>
 		</a>
 	</p>
 	<p>
 		<?php echo get_the_excerpt(); ?>
-		<a class="pull-right btn btn-primary btn-med read-more-button" href="<?php the_permalink(); ?>">
-			<?php _e( 'Read more' , 'adapter-wp' ); ?><span class="glyphicon glyphicon-chevron-right"></span>
+		<a class="pull-right btn btn-primary btn-med read-more-button" href="<?php esc_url( get_the_permalink() ); ?>">
+			<?php esc_html_e( 'Read more' , 'adapter-wp' ); ?>
+			<span class="glyphicon glyphicon-chevron-right"></span>
 		</a>
 		<div class="clearfix"></div>
 	</p>
