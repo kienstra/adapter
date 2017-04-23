3d809bec1e13407aa0490711e89a8e4eca8db2b1
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-04-22 19:10:40 -0500

    Move wp_reset_postdata() into the have_posts() block.
    Before, this was in a block outside of a loop.
    Move it, so that it runs after the query loop completes.

--- query-post-previews.php
+++ query-post-previews.php
@@ -11,8 +11,8 @@ if ( have_posts() ) {
 		get_template_part( 'content', 'post-preview' );
 	}
 	AWP_Theme::paginate_links();
+	wp_reset_postdata();
 } else {
 	get_template_part( 'no-post-found' );
 	get_template_part( 'awp-posts-and-pages' );
-	wp_reset_postdata();
 }
