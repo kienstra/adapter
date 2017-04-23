7c7ec56c3c742f666f77cd5bde34f81be40dd4cb
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-04-22 19:29:09 -0500

    Use implode() to output post classes.
    Props Thierry Muller, who showed this in a different context.
    And escape the author_date_category_tag() method.

--- content-post-preview.php
+++ content-post-preview.php
@@ -1,6 +1,6 @@
 <?php defined( 'ABSPATH' ) or die( 'No direct access!' ); ?>
 
-<article <?php esc_attr( get_post_class( 'post-preview' ) ); ?>>
+<article class="<?php esc_attr( implode( ' ', array_filter( get_post_class( 'post-preview' ) ) ) ); ?>">
 	<a href="<?php echo esc_url( get_the_permalink() ); ?>">
 	<?php the_post_thumbnail( array( 300, 200 ), array( 'class' => 'pull-right img-rounded home-wp-post-image home-featured-image' ) ); ?>
 	</a>
@@ -8,7 +8,7 @@
 		<a href="<?php echo esc_url( get_the_permalink() ); ?>"><?php the_title(); ?></a>
 	</h2>
 	<p>
-		<?php AWP_Theme::author_date_category_tag(); ?>
+		<?php wp_kses_post( AWP_Theme::get_author_date_category_tag() ); ?>
 		<a href="<?php echo esc_url( get_the_permalink() ); ?>" class="mobile-post-permalink">
 	<span class="glyphicon glyphicon-chevron-right"></span>
 		</a>
