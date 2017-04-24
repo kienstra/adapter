78e81d3c20f2b14a0e4e2125043bd0cf3bf3b6c0
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-04-23 19:56:27 -0500

    Use get_the_post_thumbnail in order to escape inline.
    Before, this used the_post_thumbnail, which echoed the result.
    Also, echo get_the_excerpt(), after escaping it.

--- content-post-preview.php
+++ content-post-preview.php
@@ -2,19 +2,24 @@
 
 <article class="<?php esc_attr( implode( ' ', array_filter( get_post_class( 'post-preview' ) ) ) ); ?>">
 	<a href="<?php echo esc_url( get_the_permalink() ); ?>">
-	<?php the_post_thumbnail( array( 300, 200 ), array( 'class' => 'pull-right img-rounded home-wp-post-image home-featured-image' ) ); ?>
+		<?php
+		$thumbnail_args = array(
+			'class' => 'pull-right img-rounded home-wp-post-image home-featured-image',
+		);
+		echo wp_kses_post( get_the_post_thumbnail( get_the_ID(), array( 300, 200 ), $thumbnail_args ) );
+		?>
 	</a>
 	<h2>
 		<a href="<?php echo esc_url( get_the_permalink() ); ?>"><?php the_title(); ?></a>
 	</h2>
 	<p>
-		<?php wp_kses_post( AWP_Theme::get_author_date_category_tag() ); ?>
+		<?php echo wp_kses_post( AWP_Theme::get_author_date_category_tag() ); ?>
 		<a href="<?php echo esc_url( get_the_permalink() ); ?>" class="mobile-post-permalink">
-	<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="glyphicon glyphicon-chevron-right"></span>
 		</a>
 	</p>
 	<p>
-		<?php echo get_the_excerpt(); ?>
+		<?php echo wp_kses_post( get_the_excerpt() ); ?>
 		<a class="pull-right btn btn-primary btn-med read-more-button" href="<?php echo esc_url( get_the_permalink() ); ?>">
 			<?php esc_html_e( 'Read more' , 'adapter-wp' ); ?>
 			<span class="glyphicon glyphicon-chevron-right"></span>
