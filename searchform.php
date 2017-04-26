f05f46718cebd188ce035ff31f55dfb75e775e7b
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-04-25 20:02:02 -0500

    Replace $_GET usage with get_query_var.
    Before, this was using the $_GET global without nonce verification.
    Store this in a variable, and output it in the <input>.

--- searchform.php
+++ searchform.php
@@ -5,11 +5,12 @@
  * @package AdapterTheme
  */
 
+$search_terms = get_query_var( 's' );
 ?>
 <form role="form" action="<?php echo esc_url( get_home_url() ); ?>" method="get" id="search-form">
 	<label for="s" class="sr-only"><?php esc_html_e( 'Search', 'adapter-wp' ); ?></label>
 	<div class="input-group">
-		<input type="text" id="s" class="form-control" name="s" placeholder="Search" <?php echo ( $search_terms ) ? ' value="' . esc_html( $search_terms ) . '"' : null; ?> />
+		<input type="text" id="s" class="form-control" name="s" placeholder="<?php esc_html_e( 'Search', 'adapter-wp' ); ?>" <?php echo ( $search_terms ) ? ' value="' . esc_attr( $search_terms ) . '"' : null; ?> />
 		<div class="input-group-btn">
 			<button type="submit" class="btn btn-primary btn-med">
 				<span class="glyphicon glyphicon-search"></span>
