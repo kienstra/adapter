77f3de85e2ee379512f8a0214cee48a695dd2ded
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-04-25 20:15:13 -0500

    Improve formatting of single.php.
    Mainly whitespace edits, including proper indentation.
    Also, change filedoc for sidebar.php.

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Form for searching the site.
+ * Main sidebar.
  *
  * @package AdapterTheme
  */
--- single.php
+++ single.php
@@ -10,27 +10,33 @@ get_header();
 ?>
 <div class="row">
 	<div class="col-md-8"> 
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content' , get_post_format() ); ?>
-			<?php AWP_Theme::custom_wp_link_pages(); ?>
-			<hr>
-			<?php do_action( 'awp_after_full_single_post_content' ); ?>
-			<?php comments_template(); ?>
-			<ul class="pager">
-				<?php echo next_post_link( '<li>%link</li>' , '<span class="glyphicon glyphicon-chevron-left"></span>&nbsp;%title' ); ?>
-				<?php echo previous_post_link( '<li>%link</li>' , '%title&nbsp;<span class="glyphicon glyphicon-chevron-right"></span>' ); ?>
-			</ul>
-		<?php endwhile; else :
+		<?php if ( have_posts() ) :
+			while ( have_posts() ) :
+				the_post();
+				get_template_part( 'content' , get_post_format() );
+				AWP_Theme::custom_wp_link_pages();
+				?>
+				<hr>
+				<?php do_action( 'awp_after_full_single_post_content' ); ?>
+				<?php comment_form(); ?>
+				<ul class="pager">
+					<?php next_post_link( '<li>%link</li>' , '<span class="glyphicon glyphicon-chevron-left"></span>&nbsp;%title' ); ?>
+					<?php previous_post_link( '<li>%link</li>' , '%title&nbsp;<span class="glyphicon glyphicon-chevron-right"></span>' ); ?>
+				</ul>
+				<?php
+			endwhile;
+		else :
 			get_template_part( 'no-post-found' );
 			get_template_part( 'awp-posts-and-pages' );
 		endif;
 		?>
 	</div>
-	<div class="col-md-4 main-sidebar"> <!--span4 -->
-		<?php wp_meta(); ?>	 
-		<?php if ( is_active_sidebar( 'main_sidebar' ) ) : ?>
-			<?php dynamic_sidebar( 'main_sidebar' ); ?>
-		<?php endif; ?>
+	<div class="col-md-4 main-sidebar">
+		<?php wp_meta();
+		if ( is_active_sidebar( 'main_sidebar' ) ) :
+			dynamic_sidebar( 'main_sidebar' );
+		endif;
+		?>
 	</div>
 </div>
 <?php get_footer();
