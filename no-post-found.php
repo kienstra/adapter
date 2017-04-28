a942c6ee6b57c29cf123f60e26f58ba4104ff99b
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-04-28 17:28:33 -0500

    Use esc_html_e, as the result will be HTML.
    Before, this used esc_attr_e.
    But this needs to strip HTML tags, not prepare the value as an attribute.

--- no-post-found.php
+++ no-post-found.php
@@ -1,4 +1,4 @@
-<?php
+j<?php
 /**
  * Display if no posts are found.
  *
@@ -10,10 +10,10 @@
 	<div class="container">
 		<h2>
 			<span class="glyphicon glyphicon-exclamation-sign"></span>
-			<?php esc_attr_e( 'Sorry, we could not find your page.', 'adapter-wp' ); ?>
+			<?php esc_html_e( 'Sorry, we could not find your page.', 'adapter-wp' ); ?>
 		</h2>
 		<h4>
-			<?php esc_attr_e( 'Try a search, or click a link below.' , 'adapter-wp' ); ?>
+			<?php esc_html_e( 'Try a search, or click a link below.' , 'adapter-wp' ); ?>
 		</h4>
 		<div class="row">
 			<div class="col-md-5">
