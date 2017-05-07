187a9e76208636e7b754d71f07767a82c55f5bbd
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-05-07 17:04:05 -0500

    Remove file that added a custom background color.
    This isn't needed in the theme.
    One can use the 'Additional CSS' area if needed.

--- functions.php
+++ functions.php
@@ -7,7 +7,6 @@
 
 include_once( get_template_directory() . '/includes/class-awp-init.php' );
 include_once( get_template_directory() . '/includes/class-awp-theme.php' );
-include_once( get_template_directory() . '/includes/class-awp-customizer.php' );
 include_once( get_template_directory() . '/includes/class-awp-admin-menu.php' );
 include_once( get_template_directory() . '/includes/wp_bootstrap_navwalker.php' );
 
