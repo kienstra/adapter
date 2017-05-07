f5013548e59972b146073c0394b2e2d9187bd24a
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-05-07 16:48:25 -0500

    Remove the filter that allows changing the navwalker path.
    There aren't many Bootstrap navwalkers.
    So it's not clear that there's a need for a filter.

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ include_once( get_template_directory() . '/includes/class-awp-init.php' );
 include_once( get_template_directory() . '/includes/class-awp-theme.php' );
 include_once( get_template_directory() . '/includes/class-awp-customizer.php' );
 include_once( get_template_directory() . '/includes/class-awp-admin-menu.php' );
-include_once( apply_filters( 'awp_navwalker_path', get_template_directory() . '/includes/wp_bootstrap_navwalker.php' ) );
+include_once( get_template_directory() . '/includes/wp_bootstrap_navwalker.php' );
 
 global $awp_init;
 $awp_init = new AWP_Init();
